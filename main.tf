aad82f79f9dcde403457dca6b003dbe672ec05a1
Dirk Breuer <mail@breuer.io>
2015-12-27 18:59:32 +0100

    Added terraform config to build OpenVPN droplet

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
+
+resource "digitalocean_droplet" "openvpn" {
+  image = "ubuntu-14-04-x64"
+  name = "32c3-openvpn"
+  region = "fra1"
+  size = "512mb"
+  ssh_keys = ["${var.do_ssh_key}"]
+  user_data = "${template_file.openvpn_userdata.rendered}"
+}
+
+resource "template_file" "openvpn_userdata" {
+  template = "openvpn.yml"
+}
+
+variable "do_token" {}
+variable "do_ssh_key" {}
+
+output "droplet_ip" {
+  value = "${digitalocean_droplet.openvpn.ipv4_address}"
+}
